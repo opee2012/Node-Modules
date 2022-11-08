8f205e4f328e54fc820df98cf742408c65ff2c20
Jacob Borth <borthj2@winthrop.edu>
2022-11-08 13:55:04 -0500

    created files

---
+++ pun.js
@@ -0,0 +1,13 @@
+var chalk = require("chalk")
+var puns = require("puns.dev")
+
+// Error checking for argument length
+
+
+// Handled case for no command line arguments for random pun
+
+// Handled case for search query
+
+// Handled case for id (NUM)
+
+// Handled case for help
