9324e565d18d137c5c2d6d4a8dae233e93f65642
opee2012 <jborth2010@gmail.com>
2022-11-15 18:51:23 -0500

    Smurf speak complete. Need to do documentation

--- smurf-speak.js
+++ smurf-speak.js
@@ -1,3 +1,20 @@
 const smurfModule = require("./smurf.dev/index.js");
 const chalk = require("chalk");
 
+if (process.argv.length < 3) {
+    console.log(chalk.redBright("Invalid input! Please try again."));
+    process.exit(1);
+}
+function filterInt(value) {
+    return /^[-+]?(\d+|Infinity)$/.test(value) ? Number(value) : NaN;
+}
+
+let inputString = process.argv[2];
+if (filterInt(inputString) === inputString) {
+    console.log(chalk.redBright("Input was an integer! It needs to be a string."));
+    process.exit(1);
+}
+
+console.log(chalk.blueBright(smurfModule.smurfConvert(inputString)));
+
+process.exit(0);
--- smurf.dev/index.js
+++ smurf.dev/index.js
@@ -10,22 +10,16 @@ exports.smurfConvert = (originalText) => {
     //orig text might be something like "I'm having a great day"
     if (originalText === undefined) return originalText;
 
+    let tempArray = originalText.split(" ");
 
     smurf.forEach(keyword => {
-        /*
-        keyword looks like
-        {
-           "word": "great",
-           "smurf": "smurfy"
+        if (!originalText.includes(keyword.word)) return originalText;
+
+        for (let i = 0; i < tempArray.length; i++) {
+            if (tempArray[i] == keyword.word) tempArray[i] = keyword.smurf;
         }
-
-        keyword.smurf
-        keyword.word
-
-         */
-        //string replacement of keyword.word for keyword.smurf
-
     });
+    convertedText = tempArray.join(" ")
 
     return convertedText;
 }
